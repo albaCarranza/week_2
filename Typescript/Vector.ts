77e9589795361a001daaf1d5378c1e3c661a0808
albaCarranza <alba.carranza.g@gmail.com>
2023-05-07 12:14:53 +0200

    more changes

--- Typescript/Vector.ts
+++ Typescript/Vector.ts
@@ -1,4 +1,4 @@
-
+    
     export class Vector {
     private elements: Array<number>
     public n: number
--- Typescript/VectorTest.ts
+++ Typescript/VectorTest.ts
@@ -2,11 +2,12 @@
 import { Vector } from "./Vector";
 
 let vector : Vector = new Vector(7, 17)
+let v3 = [3, 5, 3, 6, 3, 6, 9]
 
 console.log(vector.print());
-console.log(vector.add());
-console.log(vector.subs());
-console.log(vector.mult());
+console.log(vector.add(v3));
+console.log(vector.subs(v3));
+console.log(vector.mult(v3));
 console.log(vector.multNumb(8));
 
 
