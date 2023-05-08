dcda35d10af94fc8c72828953f2573411218a2e9
albaCarranza <alba.carranza.g@gmail.com>
2023-05-08 13:56:33 +0200

    changess

--- POO/person.js
+++ POO/person.js
@@ -23,16 +23,16 @@ class Person {
         return curretYear - this.anyoNacimiento
     }
     printAll(){
-        return `nombre : ${this.nombre},${"\n"}apellido : ${this.apellido},${"\n"}anyoNacimiento : ${this.anyoNacimiento},${"\n"}weight : ${this.peso},${"\n"}height : ${this.altura},${"\n"}hobbies : ${this.hobbies}`
+        console.log(`nombre : ${this.nombre}-${"\n"}apellido : ${this.apellido}-${"\n"}anyoNacimiento : ${this.anyoNacimiento}-${"\n"}weight : ${this.peso}-${"\n"}height : ${this.altura}-${"\n"}hobbies : ${this.hobbies}`);
     }
     printHobbies(){
-        return []
+        console.log(this.hobbies);
     }
 }
 let manuel = new Person("Manuel", "Gallardo", 1996, 80, 1.90, ["comer", "dormir", "dibujar"])
 // console.log(manuel);
 // console.log(manuel.calEdad(2023));
-console.log(manuel.printAll());
+manuel.printAll();
 console.log(manuel.calEdad(2023));
 console.log(manuel.calIMC(80,1.90));
 
--- POO/personTest.js
+++ POO/personTest.js
@@ -2,7 +2,14 @@
 // • Crear un objeto de la clase Persona y probar todos sus atributos y métodos.
 // • Sube todos los cambios a tu rama “dia1”
 
-// let myLib = require("./person")
+let myLib = require("./person")
+
 let Juan = new myLib.Person("Juan", "Gallardo", 1994, 70, 1.80, ["correr", "dormir", "dibujar"])
+
+manuel.printAll();
+console.log(Juan.calEdad(2023));
+console.log(Juan.calIMC(70,1.80));
+console.log(this.hobbies);
+
 
 // console.log(Juan);
