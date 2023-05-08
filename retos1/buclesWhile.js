a37ef0a6376dceb8cbfe866810ba9f8b3afc6b48
albaCarranza <alba.carranza.g@gmail.com>
2023-05-09 00:54:33 +0200

    Retos bucles while

--- retos1/buclesWhile.js
+++ retos1/buclesWhile.js
@@ -1,49 +1,46 @@
 // Realizar una función que te devuelva si existe un numero par en el array de números que
 // introduces como parámetro de entrada.
 // La cabecera de la función tendrá el siguiente aspecto: function hasEven(myNums)
-
-function hasEven(myNums){
-    let i = 0;
+function hasEven(myNums) {
+    var i = 0;
     while (i < myNums.length && myNums[i] % 2 != 0) {
         i++;
     }
     if (i == myNums.length) {
         return "No hay pares";
-    } else { 
-        return "Se han encontrado pares, y el primero es: " + myNums[i]; 
+    }
+    else {
+        return "Se han encontrado pares, y el primero es: " + myNums[i];
     }
 }
-
-console.log('---Indicar si existe un numero par----')
-let numeros = [5, 4, 3, 6, 8];
+console.log('---Indicar si existe un numero par----');
+var numeros = [5, 4, 3, 6, 8];
 console.log(hasEven(numeros));
-
 // Realizar una función que reciba un array de nombres y te devuelva verdadero si y solo si
 // todos los nombres empiezan por M.
 // La cabecera de la función tendrá el siguiente aspecto: function startWithM(myNames)
-function startWithM(myNames){
-    let i = 0;
-    let encontrados = 0;
+function startWithM(myNames) {
+    var i = 0;
+    var encontrados = 0;
     while (i < myNames.length) {
-        if(myNames[i][0] === 'M'){
+        if (myNames[i][0] === 'M') {
             encontrados++;
         }
         i++;
     }
-
     if (myNames.length == encontrados) {
         return true;
-    } else { 
+    }
+    else {
         return false;
     }
 }
-
-console.log('---Indicar si todos los nombres empiezan por M----')
-let nombres = ['María','Marcelo','Marta'];
-
-let resultM = startWithM(nombres);
-if(resultM == true){
+console.log('---Indicar si todos los nombres empiezan por M----');
+var nombres = ['María', 'Marcelo', 'Marta', 'Paco'];
+var resultM = startWithM(nombres);
+if (resultM == true) {
     console.log('Todos los nombres empiezan por M');
-}else{
+}
+else {
     console.log('NO todos los nombres empiezan por M');
 }
