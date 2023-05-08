1ab01509d15c6949c17e4948956e44c6a3af3d9e
albaCarranza <alba.carranza.g@gmail.com>
2023-05-09 00:36:38 +0200

    Retos bucles for

--- retos1/buclesFor.js
+++ retos1/buclesFor.js
@@ -2,16 +2,14 @@
 // indicado como parámetro de entrada.
 // La cabecera de la función tendrá el siguiente aspecto: function evenNumbers(num)
 function evenNumbers(num) {
-    num: Number;
-    var contador;
     for (var i = 0; i <= num; i++) {
         if ((i % 2) !== 0) {
-            contador = i;
+            console.log(i);
         }
     }
-    return contador;
 }
-console.log(evenNumbers(50));
+console.log('---Numeros impares hasta el 50----');
+evenNumbers(50);
 // Realizar una función que como parámetro de entrada reciba un array y como salida
 // devuelva el array revertido. No se puede utilizar el método revert de la clase array
 // La cabecera de la función tendrá el siguiente aspecto: function myRevert(myArr)
@@ -24,27 +22,43 @@ function myRevert(myArr) {
 }
 var myArr = [6, 5, 4, 3, 2, 1, 0];
 var revert = myRevert(myArr);
+console.log('---Array inverso----');
 console.log(revert);
 // Realizar una función que reciba como parámetro un array de strings que contenga
 // nombres de colores y te imprima en cada caso si el color está en el arcoíris o no.
 // La cabecera de la función tendrá el siguiente aspecto: function isRainbow(colors)
 function isRainbow(colors) {
-    var colores;
-    colores = ["rojo", "naranja", "amarillo", "verde", "añil", "azul", "violeta"];
-    for (var i = 0; i < colores.length; i++) {
-        if (colores[i] === "amarillo") {
-            return colors;
+    var coloresArcoIris = ["rojo", "naranja", "amarillo", "verde", "añil", "azul", "violeta"];
+    for (var _i = 0, colors_1 = colors; _i < colors_1.length; _i++) {
+        var element = colors_1[_i];
+        var encontrado = false;
+        for (var _a = 0, coloresArcoIris_1 = coloresArcoIris; _a < coloresArcoIris_1.length; _a++) {
+            var element2 = coloresArcoIris_1[_a];
+            if (element == element2) {
+                encontrado = true;
+            }
+        }
+        if (encontrado == true) {
+            console.log("El color " + element + " pertenece al arcoiris");
+        }
+        else {
+            console.log("El color " + element + " NO pertenece al arcoiris");
         }
     }
 }
-var colores = isRainbow(Boolean);
-console.log(colores);
+console.log('---Array de colores del arcorires----');
+var colors = ['morado', 'negro', 'naranja', 'burdeos', 'añil'];
+isRainbow(colors);
 // Realizar una función que te devuelva la suma del numero de caracteres de las palabras
 // almacenadas en un array.
 // La cabecera de la función tendrá el siguiente aspecto: function add(myWords)
 function add(myWords) {
-    var total = 0, numeros = [1, 2, 3, 4, 5];
-    for (var i = 0; i < numeros.length; i++)
-        total += numeros[i];
-    console.log(total);
+    var total = 0;
+    for (var i = 0; i < myWords.length; i++) {
+        total = total + myWords[i].length;
+    }
+    return total;
 }
+var array = ["fewe", "fefg", "fergre"];
+console.log('---Suma longitud de palabras----');
+console.log(add(array));
--- retos1/buclesFor.ts
+++ retos1/buclesFor.ts
@@ -3,62 +3,75 @@
 // La cabecera de la función tendrá el siguiente aspecto: function evenNumbers(num)
 
 function evenNumbers(num) {
-    num: Number
-    let contador;
     for (let i = 0; i <= num; i++) {
         if ((i % 2) !== 0) {
-            contador = i;
+            console.log(i);
         }
     }
-    return contador;
 }
-console.log(evenNumbers(50));
+console.log('---Numeros impares hasta el 50----')
+evenNumbers(50);
 
 // Realizar una función que como parámetro de entrada reciba un array y como salida
 // devuelva el array revertido. No se puede utilizar el método revert de la clase array
 // La cabecera de la función tendrá el siguiente aspecto: function myRevert(myArr)
 
-    function myRevert(myArr) {
-        let revert = new Array;
-        for (let i = myArr.length - 1; i >= 0; i--) {
-            revert.push(myArr[i]);
-        }
-        return revert;
+function myRevert(myArr) {
+    let revert = new Array;
+    for (let i = myArr.length - 1; i >= 0; i--) {
+        revert.push(myArr[i]);
     }
+    return revert;
+}
 
-    let myArr = [ 6, 5, 4, 3, 2, 1, 0 ];
-    let revert = myRevert(myArr);
-    console.log(revert);
+let myArr = [ 6, 5, 4, 3, 2, 1, 0 ];
+let revert = myRevert(myArr);
+
+console.log('---Array inverso----')
+console.log(revert);
 
 // Realizar una función que reciba como parámetro un array de strings que contenga
 // nombres de colores y te imprima en cada caso si el color está en el arcoíris o no.
 // La cabecera de la función tendrá el siguiente aspecto: function isRainbow(colors)
 
-
 function isRainbow(colors){
-let colores : string[];
-colores = ["rojo", "naranja", "amarillo", "verde", "añil", "azul", "violeta"]
+    const coloresArcoIris = ["rojo", "naranja", "amarillo", "verde", "añil", "azul", "violeta"]
 
-for (let i = 0; i < colores.length; i++)
-{
-    if (colores[i] === "amarillo"){
-    return colors;
-} 
-}}
-let colores = isRainbow()
-console.log(colores);
+    for (let element of colors) {
+        let encontrado = false;
+        for (let element2 of coloresArcoIris) {
+            if (element == element2) {
+                encontrado = true;
+            }
+        }
+        if(encontrado == true){
+            console.log("El color " + element + " pertenece al arcoiris");
+        } else {
+            console.log("El color " + element + " NO pertenece al arcoiris")}
+    }
+}
+
+
+console.log('---Array de colores del arcorires----')
+let colors = ['morado','negro','naranja','burdeos','añil']
+isRainbow(colors);
 
 
 // Realizar una función que te devuelva la suma del numero de caracteres de las palabras
 // almacenadas en un array.
 // La cabecera de la función tendrá el siguiente aspecto: function add(myWords)
 
-function add(myWords)
-{
-    let total= []
-    let array = ["fewe", "fefg", "fergre"];
-    for(let i = 0; i < array.length; i++) 
-    if(total +=array[i];)
+function add(myWords) {
+    let total = 0
 
-    console.log(total);
+    for (let i = 0; i < myWords.length; i++) {
+        total = total + myWords[i].length
+    }
+    return total;
 }
+
+let array = ["fewe", "fefg", "fergre"];
+
+console.log('---Suma longitud de palabras----')
+
+console.log(add(array));
